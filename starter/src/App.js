54a8118ebfad627f757d415a679c6be8075068e8
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2022-04-26 13:28:05 +0200

    useEffect keeping track on store.cart.cartItems when changes

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,28 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import {calculateTotals} from './features/cart/cartSlice'
+import {useSelector, useDispatch} from 'react-redux'
+import {useEffect} from "react"
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+ 
+  const dispatch = useDispatch()
+
+  const {cartItems} = useSelector(store => store.cart)
+
+  // everytime cartItems changes (in this case it's keeping track on calculateTotals reducer)
+  // useEffect will be invoke because the dependecies array is looking at cartItems
+
+  useEffect(()=>{
+    dispatch(calculateTotals())
+    //use Effect mira al array cartItems.
+  }, [dispatch, cartItems])
+  
+  return (
+    <>
+      <Navbar />
+      <CartContainer />
+    </>
+  );
 }
 export default App;
